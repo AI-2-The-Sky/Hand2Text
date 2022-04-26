57da30e427c8f317772a8ec7da4d75450d0f754f
Paul Dal molin <pdal-mol@z3r11p1.42lyon.fr>
2022-04-26 16:53:11 +0200

    feat(models) adapt image classifier to various input shapes

--- src/models/components/simple_cnn.py
+++ src/models/components/simple_cnn.py
@@ -5,12 +5,23 @@ from torch import nn
 from torchvision import datasets, transforms
 
 
+def first_dense_arg(channels, width, height, kernel_size):
+    x = torch.randn(channels, width, height)
+    conv1 = nn.Conv2d(channels, channels * 2, kernel_size)
+    pool = nn.MaxPool2d(2, 2)
+    conv2 = nn.Conv2d(channels * 2, channels * 4, kernel_size)
+    x = pool(F.relu(conv1(x)))
+    x = pool(F.relu(conv2(x)))
+    return x.shape[0] * x.shape[1] * x.shape[2]
+
+
 class SimpleCNNModel(nn.Module):
     def __init__(
         self,
         channels: int = 1,
         width: int = 28,
         height: int = 28,
+        kernel_size: int = 5,
         corpus: str = "/usr/share/dict/words",
     ):
         super().__init__()
@@ -18,18 +29,22 @@ class SimpleCNNModel(nn.Module):
         self.channels = channels
         self.width = width
         self.height = height
+        self.kernel_size = kernel_size
         self.corpus = np.array(open(corpus).read().splitlines())
         self.n = 10
 
-        self.conv1 = nn.Conv2d(self.channels, self.channels * 2, 5)
+        self.conv1 = nn.Conv2d(self.channels, self.channels * 2, kernel_size)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(self.channels * 2, self.channels * 4, 5)
+        self.conv2 = nn.Conv2d(self.channels * 2, self.channels * 4, kernel_size)
 
-        self.fc1 = nn.Linear(64, 120)
+        self.fc1 = nn.Linear(
+            first_dense_arg(self.channels, self.width, self.height, self.kernel_size), 120
+        )
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, self.n)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
 

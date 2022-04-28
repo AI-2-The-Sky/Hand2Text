b71a116c9d900a4f28369194d1601eb50301a61d
Gabriel Guiet-Dupré <gabriel.guiet-dupre@epitech.eu>
2022-04-28 08:29:42 +0200

    feat(split videos): program python that take 1/25 frames with opencv

---
+++ data/video_to_image.py
@@ -0,0 +1,31 @@
+import cv2, os
+
+def get_frame_from_video(filepath, name_file):
+    vid = cv2.VideoCapture(filepath)
+    current_frame = 0
+    name_dir = name_file[0:5]
+
+    while (True):
+        success, frame = vid.read()
+        if (success and current_frame % 25 == 0 and current_frame != 0):
+            full_filepath = "./frame/" + name_dir + "/frame" + str(current_frame) + ".jpg"
+            cv2.imwrite("./frame/" + name_dir + "/frame" + str(current_frame) + ".jpg", frame)
+        current_frame += 1
+        if not success:
+            break
+
+    vid.release()
+    cv2.destroyAllWindows()
+    return
+
+compt = 0
+all_file = os.listdir("./data/raw_videos/")
+len_all_file = len(all_file)
+if not os.path.exists("./frame/"):
+    os.makedir("./frame")
+for file in all_file:
+    if not os.path.exists("./frame/" + file[0:5]):
+        os.makedirs("./frame/" + file[0:5])
+    print("Extract frames from", file, "{:.2f}".format(compt * 100 / len_all_file), "%")
+    get_frame_from_video("./data/raw_videos/" + file, file)
+    compt += 1

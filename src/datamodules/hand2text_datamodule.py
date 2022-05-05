080c2eb98169d1fc080aef7505f79ed6b2673a17
Gabriel Guiet-Dupré <gabriel.guiet-dupre@epitech.eu>
2022-05-05 11:30:21 +0200

    modif(datamodule): load dataset and config file for hyperparams

--- src/datamodules/hand2text_datamodule.py
+++ src/datamodules/hand2text_datamodule.py
@@ -5,16 +5,24 @@ from pytorch_lightning import LightningDataModule
 from torch.utils.data import ConcatDataset, DataLoader, Dataset, random_split
 from torchvision.transforms import transforms
 
+from data.video_to_image import frame_meta_to_label, load_dataset
+
 
 class Hand2TextDataModule(LightningDataModule):
     def __init__(
         self,
         data_dir: str = "data/",
+        batch_size: int = 64,
+        num_workers: int = 0,
+        pin_memory: bool = False,
     ):
         super().__init__()
 
         # this line allows to access init params with 'self.hparams' attribute
         self.save_hyperparameters(logger=False)
+
+        # data transformations
+        self.transforms = transforms.Compose([transforms.ToTensor()])
 
         self.data_train: Optional[Dataset] = None
         self.data_val: Optional[Dataset] = None
@@ -26,7 +34,9 @@ class Hand2TextDataModule(LightningDataModule):
         This method is called only from a single GPU.
         Do not use it to assign state (self.x = y).
         """
-        ...
+        # Cut video to images
+        # dvc pull ??
+        load_dataset()
 
     def setup(self, stage: Optional[str] = None) -> None:
         """Load data.
@@ -37,25 +47,45 @@ class Hand2TextDataModule(LightningDataModule):
         whether it's called before trainer.fit()` or `trainer.test()`.
         """
 
+        # Load dataset
+        dataset = load_dataset()
+
         # Assign train/val split(s) for use in Dataloaders
         if stage in (None, "fit"):
-            self.data_train, self.data_val = None, None  # Placeholders
+            data_train = filter(lambda x: x[1].split == "train", dataset)
+            self.data_train = frame_meta_to_label(data_train)
+
+            data_val = filter(lambda x: x[1].split == "val", dataset)
+            self.data_val = frame_meta_to_label(data_val)
 
         # Assign test split(s) for use in Dataloaders
         if stage in (None, "test"):
-            self.data_test = None  # Placeholder
+            data_test = filter(lambda x: x[1].split == "test", dataset)
+            self.data_test = frame_meta_to_label(data_test)
 
     def train_dataloader(self) -> DataLoader:
         return DataLoader(
             dataset=self.data_train,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=True,
         )
 
     def val_dataloader(self) -> DataLoader:
         return DataLoader(
             dataset=self.data_val,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
         )
 
     def test_dataloader(self) -> DataLoader:
         return DataLoader(
             dataset=self.data_test,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
         )

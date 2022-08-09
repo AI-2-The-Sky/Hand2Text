48f237fadc0026ad501ce638a926bc905ba13fe0
DolMalin <dolmalinn@gmail.com>
2022-08-09 16:21:58 +0200

    feat(src): add hugingface space

---
+++ src/trainers/huggingface_trainer.py
@@ -0,0 +1,22 @@
+# from types import NoneType
+
+import transformers
+import pytorch_lightning as pl
+
+class HuggingFace_Trainer(pl.Trainer):
+	def __init__(
+		self, 
+		output_dir, 
+		max_epochs: int = 10, 
+		gpus:int = 0,
+		detect_anomaly: bool = True,
+		track_grad_norm: int = 2,
+		callbacks: str = None
+	):
+		super().__init__()
+		self.max_epochs = max_epochs
+		self.gpus = gpus
+		self.detect_anomaly = detect_anomaly
+		self.track_grad_norm = track_grad_norm
+		self.callbacks = callbacks
+		self.args = transformers.TrainingArguments(output_dir=output_dir, push_to_hub=True)
